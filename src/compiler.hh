0308b55d9814d7938e4f0486b13477837bde6734
baltoli <bsc28@cam.ac.uk>
2016-12-09 22:18:41 +0000

    Set up includes and basic data in compiler

--- src/compiler.hh
+++ src/compiler.hh
@@ -1,8 +1,19 @@
-#pragma once
+#ifndef COMPILER_HH
+#define COMPILER_HH
+
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/IRBuilder.h>
+
+using namespace llvm;
 
 namespace Compiler {
 
 struct State {
+  LLVMContext C;
+  std::unique_ptr<Module> Mod;
+  IRBuilder<> B;
 };
 
 }
+
+#endif
